267a5624cd28b5426ecf38ba097410f79c77f16e
gabriel <gsymodiin@gmail.com>
2022-02-17 19:01:22 +0200

    puting pitcherSS in robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.components.TrigonTalonSRX;
 import frc.robot.components.TrigonXboxController;
 import frc.robot.constants.RobotConstants.DriverConstants;
+import frc.robot.subsystems.Pitcher.PitcherSS;
 import frc.robot.subsystems.intake.IntakeOpenerSS;
 import frc.robot.subsystems.intake.IntakeSS;
 import frc.robot.subsystems.swerve.SupplierDriveCMD;
@@ -18,6 +19,7 @@ public class RobotContainer {
     private SwerveSS swerveSS;
     private IntakeSS intake;
     private IntakeOpenerSS intakeOpener;
+    private PitcherSS pitcherSS;
 
     // Commands
     private SupplierDriveCMD driveWithXboxCMD;
@@ -41,6 +43,7 @@ public class RobotContainer {
         swerveSS = new SwerveSS();
         intake = new IntakeSS();
         intakeOpener = new IntakeOpenerSS();
+        pitcherSS = new PitcherSS();
     }
 
     /**
